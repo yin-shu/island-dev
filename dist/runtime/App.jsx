dfdd9d382dea1f1b39ee5d81e90f3a993dfcebd1
yinshu <yinshu@rongcloud.cn>
2023-01-07 22:04:42 +0800

    chore: update

--- dist/runtime/App.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.App = void 0;
-const theme_default_1 = require("../theme-default");
-function App() {
-    return <theme_default_1.Layout />;
-}
-exports.App = App;
