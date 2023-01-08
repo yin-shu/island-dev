45826fa4a8d15dca7b6bb3516b329d3581fa137a
yinshu <yinshu@rongcloud.cn>
2023-01-08 19:45:31 +0800

    feat: 配置文件解析器

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,5 +1,4 @@
 import cac from 'cac';
-import { createDevServer } from './dev';
 import { build } from './build';
 import { resolve } from 'path';
 // import { resolve } from 'root'
@@ -8,9 +7,16 @@ const cli = cac('island').version('0.0.1').help();
 
 cli.command('dev [root]', 'start dev server').action(async (root: string) => {
   console.log('dev', root);
-  const server = await createDevServer(root);
-  await server.listen();
-  server.printUrls();
+  const createServer = async () => {
+    const { createDevServer } = await import('./dev');
+    const server = await createDevServer(root, async () => {
+      await server.close();
+      await createServer();
+    });
+    await server.listen();
+    server.printUrls();
+  };
+  await createServer();
 });
 
 cli
