61273b5ef7ad94280a376fe5fc9022e02ec8923a
yinshu <yinshu@rongcloud.cn>
2023-01-08 11:41:24 +0800

    chore: update

--- src/node/build.ts
+++ src/node/build.ts
@@ -1,16 +1,16 @@
-import { join } from 'path'
-import { build as viteBuild, InlineConfig } from 'vite'
-import { CLIENT_ENTRY_PATH, SERVER_ENTRY_PATH } from './constants'
-import fs from 'fs-extra'
-import { pathToFileURL } from 'url'
-import type { RollupOutput } from 'rollup'
-import pluginReact from "@vitejs/plugin-react";
+import { join } from 'path';
+import { build as viteBuild, InlineConfig } from 'vite';
+import { CLIENT_ENTRY_PATH, SERVER_ENTRY_PATH } from './constants';
+import fs from 'fs-extra';
+import { pathToFileURL } from 'url';
+import type { RollupOutput } from 'rollup';
+import pluginReact from '@vitejs/plugin-react';
 
 export async function build(root: string = process.cwd()) {
-  const [ clientBundle, serverBundle ] = await bundle(root)
-  const serverEntryPath = join(root, '.temp', 'ssr-entry.js')
-  const { render } = (await import(pathToFileURL(serverEntryPath).toString()))
-  renderPage(render, root, clientBundle)
+  const [clientBundle, serverBundle] = await bundle(root);
+  const serverEntryPath = join(root, '.temp', 'ssr-entry.js');
+  const { render } = await import(pathToFileURL(serverEntryPath).toString());
+  renderPage(render, root, clientBundle);
 }
 
 export async function bundle(root: string) {
@@ -24,22 +24,22 @@ export async function bundle(root: string) {
         input: isServer ? SERVER_ENTRY_PATH : CLIENT_ENTRY_PATH,
         output: {
           format: isServer ? 'cjs' : 'esm'
-        },
-      },
+        }
+      }
     },
-    plugins: [ pluginReact() ]
-  })
+    plugins: [pluginReact()]
+  });
 
-  console.log(`Building client + server bundles...`);
+  console.log('Building client + server bundles...');
 
   try {
-    const [ clientBundle, serverBundle ] = await Promise.all([
+    const [clientBundle, serverBundle] = await Promise.all([
       viteBuild(resolveViteConfig(false)),
-      viteBuild(resolveViteConfig(true)),
-    ])
-    return [ clientBundle, serverBundle ]as [RollupOutput, RollupOutput]
+      viteBuild(resolveViteConfig(true))
+    ]);
+    return [clientBundle, serverBundle] as [RollupOutput, RollupOutput];
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
@@ -48,8 +48,10 @@ export async function renderPage(
   root: string,
   clientBundle: RollupOutput
 ) {
-  const clientChunk = clientBundle.output.find(chunk => chunk.type === 'chunk' && chunk.isEntry)
-  console.log(`Rendering page in server side...`);
+  const clientChunk = clientBundle.output.find(
+    (chunk) => chunk.type === 'chunk' && chunk.isEntry
+  );
+  console.log('Rendering page in server side...');
   const appHtml = render();
   const html = `
   <!DOCTYPE html>
@@ -65,7 +67,7 @@ export async function renderPage(
       <script type="module" src="/${clientChunk?.fileName}"></script>
     </body>
   </html>`.trim();
-  await fs.ensureDir(join(root, "build"));
-  await fs.writeFile(join(root, "build/index.html"), html);
-  await fs.remove(join(root, ".temp"));
+  await fs.ensureDir(join(root, 'build'));
+  await fs.writeFile(join(root, 'build/index.html'), html);
+  await fs.remove(join(root, '.temp'));
 }
--- src/node/plugin-island/indexHtml.ts
+++ src/node/plugin-island/indexHtml.ts
@@ -1,10 +1,10 @@
-import { Plugin } from "vite"
-import { readFile } from 'fs/promises'
-import { CLIENT_ENTRY_PATH, DEFAULT_TEMPLATE_PATH } from '../constants'
+import { Plugin } from 'vite';
+import { readFile } from 'fs/promises';
+import { CLIENT_ENTRY_PATH, DEFAULT_TEMPLATE_PATH } from '../constants';
 
 export function pluginIndexHtml(): Plugin {
   return {
-    name: "island:index-html",
+    name: 'island:index-html',
     transformIndexHtml(html) {
       return {
         html,
@@ -18,23 +18,23 @@ export function pluginIndexHtml(): Plugin {
             injectTo: 'body'
           }
         ]
-      }
+      };
     },
     configureServer(server) {
       return () => {
-        server.middlewares.use(async(req, res, next) => {
+        server.middlewares.use(async (req, res, next) => {
           // 1. 读取 template.HTML 的内容
-          let content = await readFile(DEFAULT_TEMPLATE_PATH, 'utf-8')
+          let content = await readFile(DEFAULT_TEMPLATE_PATH, 'utf-8');
           content = await server.transformIndexHtml(
             req.url,
             content,
             req.originalUrl
-          )
+          );
           // 2. 响应 HTML 给浏览器
-          res.setHeader('Content-Type', 'text/html')
-          res.end(content)
-        })
-      }
+          res.setHeader('Content-Type', 'text/html');
+          res.end(content);
+        });
+      };
     }
-  }
+  };
 }
